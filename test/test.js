3e81e34cdfd4faa526024aa7bcf74174d014f66a
cocobotech <141758884+cocobotech@users.noreply.github.com>
2025-11-13 13:10:30 -0800

    Create test.js

---
+++ test/test.js
@@ -0,0 +1,4 @@
+function sayHello() {
+  alert("✅ JS Loaded Successfully!\nHello from GitHub Pages 🚀");
+  document.body.style.background = "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
